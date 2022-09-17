5d619f9f07b8d44316e37d59bf5799208eedb701
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-09-17 16:25:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react';
+
+
+//              <<****************** App function starts here ****************>>
 
 function App() {
+
+  //        << ************************* JS starts here  *********************** >>
+
+
+  const [Search, setSearch] = useState([]);   // search values are stored here
+
+  const click = ()=>{
+    console.log(Search);    // function for the button
+  }
+
+
+
+  
+
+  //         << ************************* JS ends here  ************************* >>
+
+
+
+  //      << ************************* HTML starts here  *********************** >>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <h1>Muzic</h1>   {/* heading  */}
+
+      <input 
+
+        className="entry"
+        placeholder="Search for music"                           
+        type="text" value ={Search} 
+        onChange={(e)=>{setSearch(e.target.value)}}>
+
+        </input>
+
+
+
+        <button onClick={()=>{click()}}>search</button>       {/* Music Search button */}
+
+
+
+
+
+
+
+
     </div>
+    //      << ************************* HTML ends here  *********************** >>
   );
 }
 
